ea606cf2546400953937a79cd3232a2370aee241
Dominik Dawidziak <ddawidziak.dd@gmail.com>
2022-03-28 15:02:39 +0200

    setup CD

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Deployment from Github Actions 👍
         </a>
       </header>
     </div>
