fb77d290c950a7f2359aec870af180dd1d256094
Pat Bar <59734015+Joberooo@users.noreply.github.com>
2022-04-04 11:35:20 +0200

    delete Predecessors from MyToast

--- my-app/src/components/MyToastComponent/MyToast.tsx
+++ my-app/src/components/MyToastComponent/MyToast.tsx
@@ -11,7 +11,6 @@ function MyToast(state:MyToastState){
   return(
     <div className="MyToast">
       <div className='MyToastLine'>Name: <br></br>{state.rupRow.name}</div>
-      <div className='MyToastLine'>Predecessors: <br></br>{state.rupRow.predecessors}</div>
       <div className='MyToastLine'>Author: <br></br>{state.rupRow.author}</div>
       <div className='MyToastLine'>Description: <br></br>{state.rupRow.description}</div>
     </div>
